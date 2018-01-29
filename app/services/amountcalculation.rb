fd4a9347b94de78322748704e7ae9d3594725050
guillaume crouillere <gcrouillere@hotmail.fr>
2018-01-29 14:32:06 +0100

    updates before release

--- app/services/amountcalculation.rb
+++ app/services/amountcalculation.rb
@@ -13,12 +13,18 @@ class Amountcalculation
       amount_ceramique += (basketline.ceramique.price * (1 - ceramique_discount)) * basketline.quantity
       total_weight += basketline.ceramique.weight
     end
-    if total_weight > 0
+    if total_weight > 0 && total_weight < 250
+      tarif_colis = HTTParty.get(
+        "https://api.laposte.fr/tarifenvoi/v1?type=lettre&poids=#{total_weight}",
+        headers: {"X-Okapi-Key" => ENV['LAPOSTE_API_KEY'] }
+      )
+      return {total: tarif_colis[2]["price"].to_money + amount_ceramique, port: tarif_colis[0]["price"].to_money}
+    elsif total_weight >= 250
       tarif_colis = HTTParty.get(
         "https://api.laposte.fr/tarifenvoi/v1?type=colis&poids=#{total_weight}",
         headers: {"X-Okapi-Key" => ENV['LAPOSTE_API_KEY'] }
       )
-      return {total: tarif_colis[0]["price"].to_money + amount_ceramique, port: tarif_colis[0]["price"].to_money}
+      return {total: tarif_colis[2]["price"].to_money + amount_ceramique, port: tarif_colis[0]["price"].to_money}
     else
       {total: 0, port: 0}
     end
